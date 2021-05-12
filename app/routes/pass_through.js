68e75ce5d2b349b717194032115db0eda0ea7660
brwist <allgoodworks79@gmail.com>
2021-05-12 18:33:18 -0400

    Split authentication verification

--- app/routes/pass_through.js
+++ app/routes/pass_through.js
@@ -8,19 +8,14 @@ const accessKeyId = process.env.EXPERIENCE_ACCESS_KEY_ID;
 const secretAccessKey = process.env.EXPERIENCE_SECRET_ACCESS_KEY;
 
 module.exports = function() {
-
   router.get('*', (req, res, next) => {
-    if(req.isAuthenticated()) {
-      s3Proxy({
-        bucket: bucket,
-        accessKeyId: accessKeyId,
-        secretAccessKey: secretAccessKey,
-        overrideCacheControl: 'max-age=100000',
-        defaultKey: 'index.html'
-      })(req, res, next);
-    } else {
-      res.status(401).end();
-    }
+    s3Proxy({
+      bucket: bucket,
+      accessKeyId: accessKeyId,
+      secretAccessKey: secretAccessKey,
+      overrideCacheControl: 'max-age=100000',
+      defaultKey: 'index.html'
+    })(req, res, next);
   });
 
   return router;
