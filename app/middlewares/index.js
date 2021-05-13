9194d26470a175a28ad51ef564d3ee140b5a4b41
Subesh Bhandari <subeshb1@gmail.com>
2021-05-13 12:19:29 +0545

    redirect to login page on route /:client/:event

--- app/middlewares/index.js
+++ app/middlewares/index.js
@@ -7,6 +7,15 @@ function verifyAuthentication(req, res, next) {
   }
 }
 
+function clientEventAuthentication(req, res, next) {
+  if (!req.isAuthenticated()) {
+    const { client, event } = req.params
+    res.status(302).redirect(`/${client}/${event}/login`);
+  } else {
+    next();
+  }
+}
 module.exports = {
-  verifyAuthentication
+  verifyAuthentication,
+  clientEventAuthentication
 }
