1bf832407c5590e8b156bb33d9c13c2c56300478
brwist <allgoodworks79@gmail.com>
2021-05-15 20:34:29 +0545

    render url when redirect type is new_page

--- app/routes/hotspots.js
+++ app/routes/hotspots.js
@@ -10,7 +10,11 @@ module.exports = function(store) {
 
     store.retrieveRedirect(attendee.client, attendee.event, key, (redirect) => {
       if(redirect && redirect.destination_url) {
-        res.redirect(redirect.destination_url);
+        if(redirect.type === 'new_page') {
+          res.render(redirect.destination_url);
+        } else {
+          res.redirect(redirect.destination_url);
+        }
       } else {
         res.status(404).end();
       }
