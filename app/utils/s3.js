43016798e935bf28df38d65dc3901737a73cd2b9
brwist <allgoodworks79@gmail.com>
2021-05-19 23:09:05 +0545

    add function to generate presigned url

--- app/utils/s3.js
+++ app/utils/s3.js
@@ -7,6 +7,12 @@ async function downloadFromS3(s3Client, bucket, key) {
   }
 }
 
+function generatePresignedUrl(s3Client, bucket, key, duration) {
+  var params = { Bucket: bucket, Key: key, Expires: duration };
+  return s3Client.getSignedUrl('getObject', params);
+}
+
 module.exports = {
-  downloadFromS3
+  downloadFromS3,
+  generatePresignedUrl
 }
