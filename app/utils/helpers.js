6337ce7ea9e9f5e8dc3fba24456855c6011b9e06
brwist <allgoodworks79@gmail.com>
2021-05-27 01:08:58 +0545

    remove unused code

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,13 +1,3 @@
-const eventStartConfigKey = 'start_time';
-const eventEndConfigKey = 'end_time';
-
-async function isEventLive(store, client, event) {
-  const startTime = await fetchEventConfig(store, client, event, eventStartConfigKey);
-  const endTime = await fetchEventConfig(store, client, event, eventEndConfigKey);
-  const now = new Date().getTime();
-  return now >= parseInt(startTime) && now <= parseInt(endTime) ? true : false;
-}
-
 function fetchEventConfig(store, client, event, configKey) {
   return new Promise((resolve, reject) => {
     // get event configuration from Redis
@@ -51,7 +41,6 @@ async function fetchDefaultRedirect(store) {
 }
 
 module.exports = {
-  isEventLive,
   fetchEventConfig,
   fetchSystemConfig,
   fetchLoginBackground,
