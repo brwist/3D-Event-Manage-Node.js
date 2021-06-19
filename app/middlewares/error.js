6a81d57c995e324f0f46a1783626784fa3a14363
brwist <allgoodworks79@gmail.com>
2021-06-18 21:58:13 -0400

    Fix crash when handling error, Use system styling color for errors

--- app/middlewares/error.js
+++ app/middlewares/error.js
@@ -1,15 +1,14 @@
 const debug = require('debug')('router-error');
-const { fetchEventConfig } = require('../utils/helpers')
+const { fetchStylingColor } = require('../utils/helpers')
 module.exports = {
   handle: (store) => {
     return (async (error, req, res, next) => {
       debug('Error accessing %s -- %O', req.originalUrl, error);
       if(error.status != 404) {
         // let default handler take care of unknown error
-        throw error;
+        await Promise.resolve(function() { throw error; });
       }
-      const { client, event } = req.params;
-      res.locals.backgroundColor = await fetchEventConfig(store, client, event, 'landing_background_color');
+      res.locals.backgroundColor = await fetchStylingColor(store);
       return res.type('.html').status(404).render('404');
     });
   }
